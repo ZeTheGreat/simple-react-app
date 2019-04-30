4a9940a5ecd51ffd3f1ddb2f0d075cee7308260a
alexislopes <alexisdrops@gmail.com>
2019-04-30 10:38:07 -0300

    fixed order

--- jenkins/scripts/publish.sh
+++ jenkins/scripts/publish.sh
@@ -2,10 +2,11 @@ echo 'Nesta etapa o Jenkins irá publicar o código na branch "prod" do reposit�
 
 
 set -x
-echo 'set user'
-git config user.email alexisdrops@gmail.com
 echo 'set email'
+git config user.email alexisdrops@gmail.com
+echo 'set user'
 git config user.name alexislopes
+
 git checkout develop
 git merge origin/master
 git remote set-url origin git@github.com:alexislopes/simple-react-app
